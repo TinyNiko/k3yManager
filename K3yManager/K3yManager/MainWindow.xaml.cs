1ef02500dbb07119fbfba297c1fbaf6221e6f8be
TinyNiko <qwedd80@sina.com>
2015-08-13 21:18:03 +0800

    MMMMMMMMMMM
    Main framework is over

---
+++ K3yManager/K3yManager/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace K3yManager
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HELP_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("FIRST REGISTER\nTHEN YOU CAN LOGIN!!\n" ,"HELP" ,MessageBoxButton.OK); 
+        }
+
+        private void REGISTER_Click_1(object sender, RoutedEventArgs e)
+        {
+            Register reg = new Register() ;
+            reg.ShowDialog();  
+        }
+
+        private void LOGIN_Click(object sender, RoutedEventArgs e)
+        {
+            if(user.Text == "" || pwd.Text == "" )
+            {
+                MessageBox.Show("user and passwd can't be empty!!!", "Waring", MessageBoxButton.OK,MessageBoxImage.Warning);
+            }
+            
+            // use SHA-256 and DES encrypt  user 
+            // use MD5 and AES encrypt passwd  
+            // and check local user if not online check 
+            //TODO
+            String usernum = makeuser(user.Text);
+            String passwdnum = makepwd(pwd.Text); 
+         
+            if(checkall(usernum , passwdnum))
+            {
+                Main main = new Main();
+                main.Show();
+                this.Close(); 
+            }
+            else
+            {
+                MessageBox.Show("user or passwd is wrong", "wrong", MessageBoxButton.OK, MessageBoxImage.Error); 
+            }
+
+        }
+
+        private bool checkall(string usernum, string passwdnum)
+        {//TODO
+
+            return true ; 
+        }
+
+        private string makepwd(string p)
+        {
+            return "niko"; 
+        }
+
+        private string makeuser(string p)
+        {
+            return "123" ;
+        }
+    }
+}
---
+++ K3yManager/K3yManager/RegisterUI.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace K3yManager
+{
+    /// <summary>
+    /// Register.xaml 的交互逻辑
+    /// </summary>
+    public partial class Register : Window
+    {
+        private bool eq =false; 
+        private BackgroundWorker hehe;
+        public delegate int Checkall();
+        public Register()
+        {
+            InitializeComponent();
+            Thread aa = new Thread(startDelegate);
+            aa.Start(); 
+           
+        }
+        public void startBackWork()
+        {
+            Backinit();
+            Backstart(); 
+        }
+
+        public void Backinit()
+        {
+            hehe = new BackgroundWorker();
+            hehe.WorkerSupportsCancellation = false;
+         
+        }
+
+        public void Backstart() 
+        {
+            hehe.RunWorkerAsync(); 
+        }
+
+        private void startDelegate()
+        {
+            while(true)
+            {
+               
+                this.Dispatcher.Invoke(new Action(checkstate));
+                if (eq == true)
+                    break; 
+            }
+        }
+
+        void   checkstate() 
+        {
+                if(username.Text != "" )
+                {      
+                    checkuser.IsChecked = true; 
+                }
+                if(passwd.Password != "")
+                {
+                    checkpwd1.IsChecked = true; 
+                }
+                if(passwd2.Password !="" && ( passwd2.Password == passwd.Password))
+                {
+                    checkpwd2.IsChecked = true; 
+                }
+                if (checkuser.IsChecked == true && checkpwd1.IsChecked == true && checkpwd2.IsChecked == true)
+                {
+                    Regbn.IsEnabled = true ;
+                   
+                }
+
+             
+        }
+        
+      
+   
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("input you information", "HELP", MessageBoxButton.OK); 
+        }
+
+        private void Regbn_Click(object sender, RoutedEventArgs e)
+        {
+            eq = true; 
+            //save message and encrypt 
+        }
+
+        private void Return_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
+
+    
+    }
+}
